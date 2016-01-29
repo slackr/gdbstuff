ca56ebb23538fe6c169644509e4c270cdb76e09b
slackr <blindk@gmail.com>
2016-01-29 13:57:20 -0500

    Create gdb.cmd

---
+++ gdb.cmd
@@ -0,0 +1,11 @@
+set disassembly-flav intel
+display/10i $eip
+display/x $eax
+display/x $ebx
+display/x $ecx
+display/x $edx
+display/x $edi
+display/x $esi
+display/x $ebp
+display/16xw $esp
+break main
